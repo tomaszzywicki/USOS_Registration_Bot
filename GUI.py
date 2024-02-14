dece91bdd475e0d9d8e6edc94103d6df1a237dc8
tomaszzywicki <tomasz.zywicki03@gmail.com>
2024-02-14 19:07:37 +0100

    add remeber me functionality

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import customtkinter as tk
+import os
 
 class NumberEntry(tk.CTkEntry):
     def __init__(self, master=None, **kwargs):
@@ -13,39 +14,58 @@ class NumberEntry(tk.CTkEntry):
             self.bell()
             return False
 
-
 class Application(tk.CTkFrame):
 
     def __init__(self, master):
-        self.padx = 10
-        self.pady = 12
+        self.px = 10
+        self.py = 12
+        self.font = ('Roboto', 12)
+        self.checkbox_var = tk.BooleanVar(value=True)
         super(Application, self).__init__(master)
         self.pack(pady=20, padx=60, fill='both', expand=True)
         self.create_widgets()
+        if os.path.exists('login_password.txt'):
+            with open('login_password.txt', 'r', encoding='utf-8') as file:
+                data = file.read().splitlines()
+                self.username_entry.insert(0, data[0])
+                self.password_entry.insert(0, data[1])
         tk.set_appearance_mode('dark')
         tk.set_default_color_theme('dark-blue')
 
     def create_widgets(self):
         label = tk.CTkLabel(self, text='Welcome to the USOS auto-register bot!', font=('Roboto', 20))
-        label.pack(pady=12, padx=10)
+        label.pack(pady=self.py, padx=self.px)
 
-        self.username_entry = tk.CTkEntry(self, placeholder_text='Username')
-        self.username_entry.pack(pady=12, padx=10)
+        self.username_entry = tk.CTkEntry(self, placeholder_text='USOS Username', font=self.font)
+        self.username_entry.pack(pady=self.py, padx=self.px)
 
-        self.password_entry = tk.CTkEntry(self, placeholder_text='Password')
-        self.password_entry.pack(pady=12, padx=10)
+        self.password_entry = tk.CTkEntry(self, placeholder_text='USOS Password', font=self.font, show='*')
+        self.password_entry.pack(pady=self.py, padx=self.px)    
 
-        self.checkbox = tk.CTkCheckBox(self, text='Remember me')
-        self.checkbox.pack(pady=12, padx=10)
+        self.checkbox = tk.CTkCheckBox(self, text='Remember me', variable=self.checkbox_var, font=self.font, )
+        self.checkbox.pack(pady=self.py, padx=self.px)
 
-        self.link_entry = tk.CTkEntry(self, placeholder_text='Enter a link to your class here...', width=200)
-        self.link_entry.pack(pady=12, padx=10)
+        self.link_entry = tk.CTkEntry(self, placeholder_text='Enter a link to your class here...', width=200, font=self.font)
+        self.link_entry.pack(pady=self.py, padx=self.px)
 
-        self.group_nr_entry = NumberEntry(self, placeholder_text='Your target group number', width=200)
-        self.group_nr_entry.pack(pady=12, padx=10)
+        self.group_nr_entry = NumberEntry(self, placeholder_text='Your target group number', width=200, font=self.font)
+        self.group_nr_entry.pack(pady=self.py, padx=self.px)
 
-        self.confirm_button = tk.CTkButton(self, text='Confirm')
-        self.confirm_button.pack(pady=12, padx=10)
+        self.confirm_button = tk.CTkButton(self, text='Confirm', command=self.confirm_action, font=self.font)
+        self.confirm_button.pack(pady=self.py, padx=self.px)
+
+
+    def create_login_password_file(self):
+        if self.checkbox_var.get():
+            with open('login_password.txt', 'w', encoding='utf-8') as file:
+                file.write(f'{self.username_entry.get()}\n{self.password_entry.get()}')
+        else:
+            if os.path.exists('login_password.txt'):
+                os.remove('login_password.txt')
+        return
+
+    def confirm_action(self):
+        self.create_login_password_file()
 
 
 def main():
